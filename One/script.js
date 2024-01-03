f4134d0c2c597db82038ae4e50d96b8f48d3223a
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-03 22:17:23 +0100

    Base variables defined

---
+++ One/script.js
@@ -0,0 +1,23 @@
+const cssVariables = {
+    Grey_800: '#434a57',
+}
+
+const DarkMode = {
+}
+
+ setTimeout(() => {
+ }, 1000);
+//  document.body.style.setProperty('--bg', cssVariables.Grey_800);
+//  document.body.style.setProperty('--bg', 'unset');
+// const population = {
+//     male: 4,
+//     female: 93,
+//     others: 10
+//   };
+  
+//   // Iterate through the object
+//   for (const key in population) {
+//     if (population.hasOwnProperty(key)) {
+//       console.log(`${key}: ${population[key]}`);
+//     }
+//   }
