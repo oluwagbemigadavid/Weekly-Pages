07a15f51f1f901f63f4f50d8e7040692f7664323
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-13 01:45:29 +0100

    Done and dusted

--- five/theme.js
+++ five/theme.js
@@ -7,14 +7,15 @@ function changeTheme() {
         document.body.style.setProperty('--text_primary', `var(--white_70)`);
         document.body.style.setProperty('--text_secondary', `var(--white_100)`);
         document.body.style.setProperty('--nav_text', `var(--white_90)`);
-        document.body.style.setProperty('--logo_text', `var(--white_50)`);
+        document.body.style.setProperty('--logo_text', `var(--white_100)`);
         document.body.style.setProperty('--border_color', `var(--white_30)`);
         document.body.style.setProperty('--placeholder', `var(--white_40)`);
         document.body.style.setProperty('--hero-img-card', `var(--hero-img-cards-dark)`);
         document.body.style.setProperty('--logos_bg', `var(--white_60)`);
         document.body.style.setProperty('--cards-bg', `var(--cs_bg_dark)`);
-        document.body.style.setProperty('--cs', `var(--cs_bg_dark)`);
+        document.body.style.setProperty('--grid-bg-lines', `var(--hero-bg-lines-dark)`);
         document.body.style.setProperty('--icons', `var(--white_90)`);
+        document.body.style.setProperty('--lines', `rgba(255, 255, 255, 0.06)`);
     } else if(theme === 'light') {
         document.body.style.setProperty('--bg', 'unset');
         document.body.style.setProperty('--text_primary', 'unset');
@@ -26,7 +27,41 @@ function changeTheme() {
         document.body.style.setProperty('--hero-img-card', 'unset');
         document.body.style.setProperty('--logos_bg', 'unset');
         document.body.style.setProperty('--cards-bg', 'unset');
-        document.body.style.setProperty('--cs', 'unset');
+        document.body.style.setProperty('--grid-bg-lines', 'unset');
         document.body.style.setProperty('--icons', 'unset');
+        document.body.style.setProperty('--lines', 'unset');
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const linesGrid = document.getElementById("linesGrid");
+
+    const horizontalLineGap = 72;
+    const verticalLineGap = 72;
+
+    // Add horizontal lines
+    for (let i = 0; i <= linesGrid.clientHeight; i += horizontalLineGap) {
+        createLine('horizontal', i);
+    }
+
+    // Add vertical lines
+    for (let i = 0; i <= linesGrid.clientWidth; i += verticalLineGap) {
+        createLine('vertical', i);
+    }
+
+    function createLine(orientation, position) {
+        const line = document.createElement("div");
+        line.className = `line ${orientation}`;
+        linesGrid.appendChild(line);
+
+        if (orientation === 'horizontal') {
+            line.style.width = "100%";
+            line.style.height = "1px";
+            line.style.top = `${position}px`;
+        } else {
+            line.style.width = "1px";
+            line.style.height = "100%";
+            line.style.left = `${position}px`;
+        }
+    }
+});
