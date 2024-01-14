436a973a1e88bf78053dfa3db90b0d4dbb172f0a
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-14 06:27:25 +0100

    update

--- five/five.js
+++ five/five.js
@@ -1,5 +1,20 @@
-
+let lastScrollY = 0
+let visible = false;
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY
+    
+    if(currentScrollY > lastScrollY) {
+        nav.style.opacity =  0
+    } else {
+        nav.style.opacity = 1
+    }
+    document.querySelector('.menu').classList.remove('active')
+    lastScrollY = currentScrollY
+})
 document.querySelector('.hamburger').addEventListener('click', () => {
+    visible = !visible
+    console.log(visible)
     document.querySelector('.menu').classList.toggle('active')
     document.querySelector('.hamburger').classList.toggle('active')
 })
