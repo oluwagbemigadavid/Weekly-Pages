aaa7c3a57224baa1ebefb2908ac347903b29409d
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-14 05:14:33 +0100

    Update five.js

--- five/five.js
+++ five/five.js
@@ -28,7 +28,7 @@ gsap.from(logos, {
         start: 'top 90%',
         end: 'top 70%',
         scrub: true,
-        markers: false
+        once: true,
     },
     opacity: 0,
     stagger: 0.1,
@@ -41,9 +41,10 @@ gsap.from(info, {
         start: 'top 90%',
         end: 'top 70%',
         scrub: true,
+        once: true,
     },
     opacity: 0,
-    stagger: 2,
+    stagger: .2,
     x: -100
 })
 
@@ -53,10 +54,12 @@ cards.forEach(image => {
             trigger: image,
             start: 'top 90%',
             end: 'top 70%',
-            scrub: true,
+            scrub: false,
+            once: true,
         },
         opacity: 0,
         stagger: 0.1,
+        duration: 1,
     })
 
     gsap.from(image, {
@@ -66,8 +69,8 @@ cards.forEach(image => {
             end: 'top -10%',
             scrub: true,
         },
-        stagger: 0.1,
-        y: 100,
+        stagger: 0.01,
+        scale: 2.5,
     })
 })
 gsap.from(save, {
@@ -76,6 +79,7 @@ gsap.from(save, {
         start: 'top 60%',
         end: 'top 40%',
         scrub: true,
+        once: true,
     },
     opacity: 0,
     stagger: 0.1,
