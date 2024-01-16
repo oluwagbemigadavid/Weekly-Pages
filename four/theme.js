5b98b0d3139385781298b8196ea93f25dc57c64d
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-16 20:12:49 +0100

    dark mode

--- four/theme.js
+++ four/theme.js
@@ -3,19 +3,14 @@ function changeTheme() {
     const theme = themeBtn.checked ? 'dark' : 'light';
 
     if(theme === 'dark') {
-        document.body.style.setProperty('--bg', `var(--dark_bg)`);
+        document.body.style.setProperty('--bg', `var(--grey_900)`);
         document.body.style.setProperty('--text_primary', `var(--white_70)`);
         document.body.style.setProperty('--text_secondary', `var(--white_100)`);
         document.body.style.setProperty('--nav_text', `var(--white_90)`);
         document.body.style.setProperty('--logo_text', `var(--white_100)`);
         document.body.style.setProperty('--border_color', `var(--white_30)`);
-        document.body.style.setProperty('--placeholder', `var(--white_40)`);
-        document.body.style.setProperty('--hero-img-card', `var(--hero-img-cards-dark)`);
-        document.body.style.setProperty('--logos_bg', `var(--white_60)`);
-        document.body.style.setProperty('--cards-bg', `var(--cs_bg_dark)`);
-        document.body.style.setProperty('--grid-bg-lines', `var(--hero-bg-lines-dark)`);
-        document.body.style.setProperty('--icons', `var(--white_90)`);
-        document.body.style.setProperty('--lines', `rgba(255, 255, 255, 0.06)`);
+        document.body.style.setProperty('--figures_cont_bg', `var(--white_30)`);
+        document.body.style.setProperty('--pricing', `var(--white_30)`);
     } else if(theme === 'light') {
         document.body.style.setProperty('--bg', 'unset');
         document.body.style.setProperty('--text_primary', 'unset');
@@ -23,12 +18,7 @@ function changeTheme() {
         document.body.style.setProperty('--nav_text', 'unset');
         document.body.style.setProperty('--logo_text', 'unset');
         document.body.style.setProperty('--border_color', 'unset');
-        document.body.style.setProperty('--placeholder', 'unset');
-        document.body.style.setProperty('--hero-img-card', 'unset');
-        document.body.style.setProperty('--logos_bg', 'unset');
-        document.body.style.setProperty('--cards-bg', 'unset');
-        document.body.style.setProperty('--grid-bg-lines', 'unset');
-        document.body.style.setProperty('--icons', 'unset');
-        document.body.style.setProperty('--lines', 'unset');
+        document.body.style.setProperty('--figures_cont_bg', 'unset');
+        document.body.style.setProperty('--hero-pricing-card', 'unset');
     }
 }
